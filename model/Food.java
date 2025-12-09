f6322082fda57fceed93667beda91889f121a77e
Tran05 <tranhuynh.01102005@gmail.com>
2025-12-09 14:25:37 +0700

    data

---
+++ model/Food.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Food {
+    private int id;
+    private String name;
+    private double price;
+    private String image;
+
+    public Food() {}
+
+    public Food(int id, String name, double price, String image) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.image = image;
+    }
+
+    // getters + setters
+}
